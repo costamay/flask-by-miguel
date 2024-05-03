1f673d7573ef31090fcc4a9f5f0a2cb3bfefae5d
costamay <titusouko@gmail.com>
2024-05-03 18:02:20 +0300

    flask tutorial

---
+++ app/models.py
@@ -0,0 +1,55 @@
+from app import db, login
+from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from hashlib import md5
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    # return db.session.get(User, int(id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(256))
+    bio = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    
+    # Relationship mapping the user to related posts
+    posts = db.relationship('Post', back_populates='user')
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+class Post(db.Model):
+    __tablename__ = "posts"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    
+    # Relationship mapping the post to related user
+    user = db.relationship('User', back_populates='posts')
+    
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+    
+        
